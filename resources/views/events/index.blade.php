f85150fbf34bfd40f7afc5794a3e495e84d40d8b
jojomoment <kamiljamaljojo@gmail.com>
2026-04-14 18:21:21 +0100

    adjusted and made starting home page

---
+++ resources/views/events/index.blade.php
@@ -0,0 +1,2 @@
+<h1>Events Page</h1>
+<p>It works 🎉</p>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('events.index');
 });
 
 Route::get('/dashboard', function () {
@@ -15,6 +15,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+
 });
+
+use App\Http\Controllers\EventController;
+
+Route::get('/events', [EventController::class, 'index']);
 
 require __DIR__.'/auth.php';
