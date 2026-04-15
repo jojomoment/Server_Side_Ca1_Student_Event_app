3b04bf90d9da322fd552f0227ab24b9c1555d035
rsunil89 <d00281353@student.dkit.ie>
2026-04-15 20:02:33 +0100

    book event user paste

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Event extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'location',
+        'event_date',
+        'capacity',
+    ];
+
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
 }
